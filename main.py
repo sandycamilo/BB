e0a2f2588aceed7235f2a7a7db456b7f1de2ad1a
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2020-04-18 19:10:59 -0700

    refactoring

--- main.py
+++ main.py
@@ -7,60 +7,72 @@ import tflearn
 import tensorflow
 import random
 import json
+import pickle
 
 with open("intents.json") as file:
     data = json.load(file)
 
 # print(data["intents"])
 
-words = []
-labels = []
-docs_x = []
-docs_y = [] # each pattern we have correlates to a specific tag
+with open("data.pickle", "rb") as file:
+    data = json.load(file)
 
-for intent in data["intents"]: # loop through every intent(dictionary) in intents 
-    for pattern in intent["patterns"]: # access dictionary in intent and gets 'patterns'
-        pattern_words = nltk.word_tokenize(pattern) # get every word in patterns- returns a list with all different words in it
-        words.extend(pattern_words) # place all tokenized words into words list by extending the list 'wrds' to words
-        docs_x.append(pattern_words) # add pattern to docs_x []
-        docs_y.append(intent["tag"]) # helps classify each pattern
+try:
+    with open("data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f)
 
-    # get all tags in intent
-    if intent["tag"] not in labels: 
-        labels.append(intent["tag"])
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = [] # each pattern we have correlates to a specific tag
 
-words = [stemmer.stem(w.lower()) for w in words if w not in "?"] # convert all words into lowercase & remove question marks
-words = sorted(list(set(words))) # remove duplicate elements and sorts the words - all in a list
+    for intent in data["intents"]: # loop through every intent(dictionary) in intents 
+        for pattern in intent["patterns"]: # access dictionary in intent and gets 'patterns'
+            pattern_words = nltk.word_tokenize(pattern) # get every word in patterns- returns a list with all different words in it
+            words.extend(pattern_words) # place all tokenized words into words list by extending the list 'wrds' to words
+            docs_x.append(pattern_words) # add pattern to docs_x []
+            docs_y.append(intent["tag"]) # helps classify each pattern
 
-labels = sorted(labels) # sort labels 
+        # get all tags in intent
+        if intent["tag"] not in labels: 
+            labels.append(intent["tag"])
 
-# how many times does each word occur - 
-# frequency goes with entry ~ matching entry with count
+    words = [stemmer.stem(w.lower()) for w in words if w not in "?"] # convert all words into lowercase & remove question marks
+    words = sorted(list(set(words))) # remove duplicate elements and sorts the words - all in a list
 
-training = [] 
-output = []
-out_empty = [0 for _ in range(len(labels))]  
+    labels = sorted(labels) # sort labels 
 
-for x, doc in enumerate(docs_x):
-    bag = []  
+    # how many times does each word occur - 
+    # frequency goes with entry ~ matching entry with count
 
-    pattern_words = [stemmer.stem(w) for w in doc] # stem all words that are in patterns  
+    training = [] 
+    output = []
+    out_empty = [0 for _ in range(len(labels))]  
 
-    for w in words:
-        if w in pattern_words: # if words exists in the current pattern that we are looping through
-            bag.append(1) # the word exists so we place a 1 , no  matter how many times it exists
-        else:
-            bag.append(0) # the word is not there so we place a 0
+    for x, doc in enumerate(docs_x):
+        bag = []  
 
-    output_row = out_empty[:] 
-    output_row[labels.index(docs_y[x])] = 1  # look through labels list & check where the tag is in that list then set that value to 1 in output row
-                                            
-    training.append(bag) 
-    output.append(output_row)
+        pattern_words = [stemmer.stem(w) for w in doc] # stem all words that are in patterns  
 
-#takes lists and changes them into arrays to feed them to model
-training = numpy.array(training) 
-output = numpy.array(output)
+        for w in words:
+            if w in pattern_words: # if words exists in the current pattern that we are looping through
+                bag.append(1) # the word exists so we place a 1 , no  matter how many times it exists
+            else:
+                bag.append(0) # the word is not there so we place a 0
+
+        output_row = out_empty[:] 
+        output_row[labels.index(docs_y[x])] = 1  # look through labels list & check where the tag is in that list then set that value to 1 in output row
+                                                
+        training.append(bag) 
+        output.append(output_row)
+
+    #takes lists and changes them into arrays to feed them to model
+    training = numpy.array(training) 
+    output = numpy.array(output)
+
+    with open("data.pickle", "wb") as f:
+        pickle.dump(words, labels, training, output), f)
 
 tensorflow.reset_default_graph()
 
@@ -72,5 +84,8 @@ net = tflearn.regression(net)
 
 model = tflearn.DNN(net)
 
+try:
+    model.load("model.tflearn")
+except:
 model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
 model.save("model.tflearn")
